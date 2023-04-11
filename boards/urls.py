1bd050ec305e9229ac85e4cc3f60f6eb3de4fc2c
DaikiShiroma <daiki20000417@gmail.com>
2023-04-11 12:30:08 +0900

    掲示板一覧、編集画面を追加

--- boards/urls.py
+++ boards/urls.py
@@ -5,4 +5,6 @@ app_name = 'boards'
 
 urlpatterns = [
     path('create_theme', views.create_theme, name='create_theme'),
+    path('list_themes', views.list_themes, name='list_themes'),
+    path('edit_theme/<int:id>', views.edit_theme, name='edit_theme'),
 ]
