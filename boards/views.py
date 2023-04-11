d2b4b7005585eda7b4791de98c3afca8f32bbdd2
DaikiShiroma <daiki20000417@gmail.com>
2023-04-11 14:28:03 +0900

    掲示板削除機能とコメント機能を追加

--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from . import forms
 from django.contrib import messages
-from . models import Themes
+from . models import Themes,Comments
 from django.http import Http404
 
 # Create your views here.
@@ -42,4 +42,37 @@ def edit_theme(request, id):
             'id': id,
         }
     )
+
+def delete_theme(request, id):
+    theme = get_object_or_404(Themes, id=id)
+    if theme.user.id != request.user.id:
+        raise Http404
+    delete_theme_form = forms.DeleteThemeForm(request.POST or None)
+    if delete_theme_form.is_valid(): # csrf check
+        theme.delete()
+        messages.success(request, '掲示板を削除しました')
+        return redirect('boards:list_themes')
+    return render(
+        request, 'boards/delete_theme.html', context={
+            'delete_theme_form': delete_theme_form,
+        }
+    )
+
+def post_comments(request, theme_id):
+    post_comment_form = forms.PostCommentForm(request.POST or None)
+    theme = get_object_or_404(Themes, id=theme_id)
+    comments = Comments.objects.fetch_by_theme_id(theme_id)
+    if post_comment_form.is_valid():
+        post_comment_form.instance.theme =theme
+        post_comment_form.instance.user = request.user
+        post_comment_form.save()
+        return redirect('boards:post_comments', theme_id=theme_id)
+    return render(
+        request, 'boards/post_comments.html', context={
+            'post_comment_form': post_comment_form,
+            'theme': theme,
+            'comments': comments,
+        }
+    )
+
 
