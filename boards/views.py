1a68852a30564f101b254f1174056f970f580b64
DaikiShiroma <daiki20000417@gmail.com>
2023-04-12 10:31:55 +0900

    ログイン時のみコメントできるようにする機能を追加

--- boards/views.py
+++ boards/views.py
@@ -63,6 +63,8 @@ def post_comments(request, theme_id):
     theme = get_object_or_404(Themes, id=theme_id)
     comments = Comments.objects.fetch_by_theme_id(theme_id)
     if post_comment_form.is_valid():
+        if not request.user.is_authenticated:
+            raise Http404
         post_comment_form.instance.theme =theme
         post_comment_form.instance.user = request.user
         post_comment_form.save()
